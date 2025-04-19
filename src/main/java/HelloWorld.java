c37643ede9a240910cef29b2399eed2e8365d725
Karamveer-Singh <32606687+Karamveer-Singh@users.noreply.github.com>
2025-04-19 12:04:05 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,2 +1,9 @@
-public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, Jenkins +
-Maven!"); } }
+// public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, Jenkins +
+// Maven!"); } }
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println(“Hello, World”);
+    }
+}
